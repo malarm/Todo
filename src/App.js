d7c9e6ea560bd1ca48b7c78a3d242605884e29bd
malarm <malarm09@gmail.com>
2019-10-14 14:45:54 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,101 +1,50 @@
-import React, {useState} from 'react';
-import Todos from './Todos';
-import Todoform from './Todoform';
-import './App.css';
-
-/*function Todo ({todo, index,completetodo,removetodo}) {
-
-  return (
-    <div style={{textDecoration: todo.isCompleted ? 'line-through' : null}}>{todo.text}
-    <button onClick={() => completetodo(index)} > Completed</button>
-    <button onClick={() => removetodo(index)} > x</button>
-    </div>
-  )
-  }*/
-
-  /*function Todoform ({addTodo}) {
-    const [value, setValue] = useState('');
-
-    const handleSubmit = e  => {
-       e.preventDefault();
-       if(!value) return;
-       addTodo(value);
-       setValue('');
-      
-    }
-    return(
-      <form onSubmit={handleSubmit}>
-         <input type="text" value ={value} onChange={e =>setValue(e.target.value)} placeholder=" add" />
-      </form>
-    )
-  }*/
+import React, { useState } from "react";
+import Todos from "./Todos";
+import Todoform from "./Todoform";
+import "./App.css";
 
 function App() {
-  const [todos, setTodos] = useState([
-    {
-      text : 'learn about react',
-      isCompleted : false
-    }, 
-    {
-      text : 'Meet ffriend for lunch',
-      isCompleted : false
-    },
-    {
-      text : 'learn about react',
-      isCompleted : false
-    }
+  const [todos, setTodos] = useState(
+    JSON.parse(localStorage.getItem("todos")) || []
+  );
 
-  ])
-
-  const addTodovalue = text => {
-    const newTodos = [...todos, {text}]
+  const addTodotext = text => {
+    const newTodos = [...todos, { text }];
     setTodos(newTodos);
-  }
+    localStorage.setItem("todos", JSON.stringify(newTodos));
+  };
 
-  const updateTodo = (text,index) => {
-    //console.log('text');
-    const newTodos = [...todos]
-    newTodos.map(newTodo =>{
-      if(newTodo.index ===index){
-        newTodo.text=index;
-      }
-    })
-  //newTodos[index].text = e.target.value;
-    setTodos(newTodos);
-  }
-  const completetodo1 = index => {
-    const newtodos =[...todos];
+  const completetodotext = index => {
+    const newtodos = [...todos];
     //newtodos [index].isCompleted = true;
-    newtodos [index].isCompleted =! newtodos [index].isCompleted;
+    newtodos[index].isCompleted = !newtodos[index].isCompleted;
     setTodos(newtodos);
-  }
+    localStorage.setItem("todos", JSON.stringify(newtodos));
+  };
 
-  const removetodo1 = index => {
-    const newtodos =[...todos];
+  const removetodotext = index => {
+    const newtodos = [...todos];
     newtodos.splice(index, 1);
     setTodos(newtodos);
-  }
-  const alltodo = index => {
-   /* const filteredalltodo = todos.filter(({ index }) => {
-      setTodos(filteredalltodo);
-    })*/
-    const newtodos =[...todos];
+    localStorage.setItem("todos", JSON.stringify(newtodos));
+  };
 
-  }
-  return(
-    <div className ="container">
-        <h1 className="header">Todos</h1>
-       
-        <Todoform addTodo = {addTodovalue} />
-      {
-        todos.map ((todo,index) => (
-          < Todos key ={index} index ={index} todo={todo} completetodo ={completetodo1} removetodo ={removetodo1} updateTodo ={updateTodo}/>
-        ))
-      }
-     
+  return (
+    <div className="todo-container">
+      <h1 className="main-heading">Todos</h1>
+
+      <Todoform addTodo={addTodotext} />
+      {todos.map((todo, index) => (
+        <Todos
+          key={index}
+          index={index}
+          todo={todo}
+          completetodo={completetodotext}
+          removetodo={removetodotext}
+        />
+      ))}
     </div>
-  )
+  );
 }
 
-
-export default App
+export default App;
