5c459ba08d5f8cf8ae88d8b96229465b5b127e4f
malarm <malarm09@gmail.com>
2019-10-14 14:46:53 +0200

    Update Todos.js

--- src/Todos.js
+++ src/Todos.js
@@ -1,41 +1,20 @@
-import React from 'react';
+import React from "react";
 
-
-function Todos ({todo,index,completetodo,removetodo,updateTodo}) {
-
-    return (
-        <div className ="collection-item" >
-          <span> <input type="checkbox"  onClick={() => completetodo(index)}/></span> 
-          
-      <span  > <input className="update" type ="text" index={todo.index} value={todo.text} onChange={e =>updateTodo(e.target.value)} style={{textDecoration: todo.isCompleted ? 'line-through' : null}} />
+function Todos({ todo, index, completetodo, removetodo }) {
+  return (
+    <div
+      className="todo"
+      style={{ textDecoration: todo.isCompleted ? "line-through" : null }}
+    >
+      <span>
+        <input type="checkbox" onClick={() => completetodo(index)} />
       </span>
-        <span className="remove"> 
-           <button onClick={() => removetodo(index)} > x</button></span>
-        
-      </div>
-       
-   
-    )
-    }
-    export default Todos  
-/*const Todos = ({item,deletetodoca }) => {
 
-    const todolist = item.length ? (
-        item.map (todoca => {
-            return(
-                <div onClick ={ () => {deletetodoca(todoca.id)}} className="collection-item" key ={todoca.id}>
-                    <span >{todoca.content} </span>    
-                </div>
-            )
-        })
-    ) : 
-    (
-        <p className="center">You have no todos left</p>
-    )
-    return (
-        <div className ="collection">
-            {todolist}
-        </div>
-    )
+      <span>{todo.text}</span>
+      <span className="remove-item-cross" onClick={() => removetodo(index)}>
+        x
+      </span>
+    </div>
+  );
 }
-export default Todos*/
+export default Todos;
