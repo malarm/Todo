ee53855c00a44e57ad814aeeb876a85b216d5c30
malarm <malarm09@gmail.com>
2019-10-14 14:46:23 +0200

    Update Todoform.js

--- src/Todoform.js
+++ src/Todoform.js
@@ -1,20 +1,27 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
-function Todoform ({addTodo,updateTodo}) {
-   const [value, setValue] = useState('');
+function Todoform({ addTodo }) {
+  const [value, setValue] = useState("");
 
-   const handleSubmit = e  => {
-      e.preventDefault();
-      if(!value) return;
-      addTodo(value);
-      setValue('');
-     
-   }
-   return(
-     <form onSubmit={handleSubmit}>
-        <input className="text" type="text" value ={value} onChange={e =>setValue(e.target.value)}  placeholder=" Type your task here" />
-     </form>
-   )
- }
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (!value) return;
+    addTodo(value);
+    setValue("");
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <label className="form-label">
+        <input
+          className="form-input"
+          type="text"
+          value={value}
+          onChange={e => setValue(e.target.value)}
+          placeholder=" Type your task here"
+        />
+      </label>
+    </form>
+  );
+}
 
-  export default Todoform
+export default Todoform;
